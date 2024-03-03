69914b69f55a9a5da084f86bb3791d4e241f1987
apisit.a <amornchanchaigul@gmail.com>
2024-03-03 11:08:22 +0700

    hackerrank datastructures array 1

--- src/hackerrank/datastructure/Test01ArrayDs.java
+++ src/hackerrank/datastructure/Test01ArrayDs.java
@@ -2,17 +2,20 @@ package hackerrank.datastructure;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Test01ArrayDs {
     public static void main(String[] args) {
-        List<Integer> res = reverseArray(Arrays.asList(1, 4, 3, 2));
+        List<Integer> input1 = Arrays.asList(1, 4, 3, 2);
+        List<Integer> res = reverseArray(input1);
+        System.out.println(input1);
         System.out.println(res);
     }
 
-    public static List<Integer> reverseArray(List<Integer> a) {
-        // Write your code here
-        List<Integer> result = new ArrayList<>();
+    public static List<Integer> reverseArray(List<Integer> parameter) {
+        List<Integer> result = new ArrayList<>(parameter);
+        Collections.reverse(result);
         return result;
     }
 
