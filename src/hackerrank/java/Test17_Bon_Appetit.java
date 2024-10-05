bd7f3a7b12ee998c88886107261fffe70e5ff172
Apisit <amornchanchaigul@gmaul.com>
2024-10-05 09:10:05 +0700

    add 17

---
+++ src/hackerrank/java/Test17_Bon_Appetit.java
@@ -0,0 +1,59 @@
+package hackerrank.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Test17_Bon_Appetit {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> bill = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int b = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.bonAppetit(bill, k, b);
+
+        bufferedReader.close();
+    }
+}
+
+class Result {
+
+    /*
+     * Complete the 'bonAppetit' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY bill
+     *  2. INTEGER k
+     *  3. INTEGER b
+     */
+
+    public static void bonAppetit(List<Integer> bill, int k, int b) {
+        int sum = 0;
+        for (int i = 0; i < bill.size(); i++) {
+            if (i != k) {
+                sum += bill.get(i);
+            }
+        }
+        sum /= 2;
+        if (sum == b) {
+            System.out.println("Bon Appetit");
+        } else {
+            System.out.println(b-sum);
+        }
+    }
+
+}
